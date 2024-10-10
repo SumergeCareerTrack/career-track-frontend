4f601599c6a24fa14cbce7af7c6af08065be0c7d
Andysm21 <102580958+Andysm21@users.noreply.github.com>
2024-10-10 14:55:55 +0300

    Implemented Ned/update/delete type in admin dashboard

---
+++ career-track-FE/src/app/components/learnings/new-update-type/new-update-type.component.ts
@@ -0,0 +1,73 @@
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { SharedDataService } from '../../../services/shared-data/shared-data.service';
+import { TypeReq } from '../../../interfaces/backend-requests';
+
+@Component({
+  selector: 'app-new-update-type',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './new-update-type.component.html',
+  styleUrl: './new-update-type.component.css'
+})
+export class NewUpdateTypeComponent {
+
+
+@Output() cancel = new EventEmitter<void>();
+@Input() id: string = '';
+@Input() updateEnabled: boolean=false;
+@Input() createEnabled: boolean=false;
+updateCreateType: FormGroup;
+
+
+constructor(
+  formBuilder: FormBuilder,
+  private sharedDataService: SharedDataService,
+  private cookieService: CookieService,
+) {
+  this.updateCreateType = formBuilder.group({
+    typeName: ['' ],
+    typeBaseScore: [ ],
+  });
+  if(this.createEnabled){
+    this.updateCreateType.get("typeName")?.setValidators([Validators.required]);
+    this.updateCreateType.get("typeBaseScore")?.setValidators([Validators.required]);
+  }
+  else if (this.updateEnabled){
+    this.updateCreateType.get("typeName")?.clearValidators();
+    this.updateCreateType.get("typeBaseScore")?.clearValidators();
+  }
+  this.updateCreateType.updateValueAndValidity();
+
+
+}
+
+onSubmit() {
+  const type: TypeReq={
+    name: this.updateCreateType.get("typeName")? this.updateCreateType.get("typeName")?.value : null,
+    baseScore: this.updateCreateType.get("typeBaseScore")? this.updateCreateType.get("typeBaseScore")?.value : null
+  }
+  if(this.updateEnabled){
+    this.sharedDataService.updateType(this.id,type).subscribe({
+      next: (response) => {
+        this.cancel.emit();
+      },
+    });
+  }
+  else if(this.createEnabled){
+    this.sharedDataService.createType(type).subscribe({
+      next: (response) => {
+        this.cancel.emit();
+      },
+    });
+  }
+}
+onCancel() {
+  this.createEnabled=false;
+  this.updateEnabled=false;
+  this.updateCreateType.reset();
+  this.cancel.emit();
+}
+
+}
