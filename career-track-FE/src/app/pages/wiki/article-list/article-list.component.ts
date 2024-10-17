c25a4943ecb4ec6f2c5f9ae214ae503b01988c3b
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-17 15:03:11 +0300

    Fixed article list update

--- career-track-FE/src/app/pages/wiki/article-list/article-list.component.ts
+++ career-track-FE/src/app/pages/wiki/article-list/article-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 import { WikiDataService } from '../../../services/wiki-data/wiki-data.service';
 import { Article, UserResponse } from '../../../interfaces/backend-requests';
 import { FilterArticlesPipe } from '../../../pipes/wikiFilter.pipe';
@@ -6,6 +6,7 @@ import { ArticleItemComponent } from "./article-item/article-item.component";
 import { SharedDataService } from '../../../services/shared-data/shared-data.service';
 import { firstValueFrom } from 'rxjs';
 import { User } from '../../../interfaces/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-list',
@@ -14,13 +15,24 @@ import { User } from '../../../interfaces/user.model';
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.css'
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
   wikiDataService = inject(WikiDataService);
   sharedDataService = inject(SharedDataService);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  destoryRef = inject(DestroyRef);
   filter: "WIKI" | "BLOG" = "WIKI";
   articles: Article[] = [];
 
   ngOnInit(): void {
+    const sub = this.route.params.subscribe(() => {
+      setTimeout(() => { this.loadArticles(); }, 0);
+    });
+
+    this.destoryRef.onDestroy(() => sub.unsubscribe());
+  }
+
+  loadArticles() {
     this.wikiDataService.getAll().subscribe(async (articlesResponse) => {
       let articlesResponseArray = articlesResponse as Article[];
 
